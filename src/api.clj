faafdbb99cc04997a4ba6ced4d06ac8b6431c262
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-05-01 23:14:56 -0300

    feat: add GET /todos endpoint

--- src/api.clj
+++ src/api.clj
@@ -1,10 +1,11 @@
 (ns api  (:require [compojure.core :refer [GET defroutes]]
                    [compojure.route :as route]
                    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-                   [ring.adapter.jetty :refer [run-jetty]]))
+                   [ring.adapter.jetty :refer [run-jetty]]
+                   [cheshire.core :refer [generate-string]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/todos" [] (generate-string {:todos []}))
   (route/not-found "Not Found"))
 
 (def app
