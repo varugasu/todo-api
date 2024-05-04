92a77534192dcd8c62ec2e37aba92072b2edab76
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-05-03 23:42:09 -0300

    feat: add xtdb

--- src/api.clj
+++ src/api.clj
@@ -1,16 +1,20 @@
-(ns api  (:require [compojure.core :refer [GET defroutes]]
+(ns api  (:require [compojure.core :refer  [GET  routes]]
                    [compojure.route :as route]
                    [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
                    [ring.adapter.jetty :refer [run-jetty]]
-                   [cheshire.core :refer [generate-string]]))
+                   [cheshire.core :refer [generate-string]]
+                   [xtdb.client :as xtc]
+                   [xtdb.api :as xt]))
 
-(defroutes app-routes
-  (GET "/todos" [] (generate-string {:todos []}))
-  (route/not-found "Not Found"))
+(defn make-routes [node]
+  (routes (GET "/todos" [] (generate-string {:todos (xt/q node '(from :todos [*]))}))
+          (route/not-found "Not Found")))
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(defn make-app [node]
+  (wrap-defaults (make-routes node) site-defaults))
 
+(def jetty-opts {:port 3000 :join? false})
 
 (defn -main []
-  (run-jetty app {:port 3000 :join? false}))
+  (with-open [node (xtc/start-client "http://localhost:6543")]
+    (run-jetty (make-app node) jetty-opts)))
