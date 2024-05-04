3131d1476aa0cd6b879559d837ac8dec588307d8
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2024-05-04 15:50:22 -0300

    feat: parse request

--- src/api.clj
+++ src/api.clj
@@ -2,13 +2,15 @@
                    [compojure.route :as route]
                    [ring.middleware.defaults :refer [wrap-defaults api-defaults]]
                    [ring.adapter.jetty :refer [run-jetty]]
-                   [cheshire.core :refer [generate-string]]
+                   [cheshire.core :refer [generate-string parse-string]]
                    [xtdb.client :as xtc]
                    [xtdb.api :as xt]))
+(defn- read-json [request]
+  (parse-string (slurp (:body request))))
 
 (defn make-routes [node]
   (routes (GET "/todos" [] (generate-string {:todos (xt/q node '(from :todos [*]))}))
-          (POST "/todos" request (println request) "OK!!")
+          (POST "/todos" request (println (read-json request)) "OK!!")
           (route/not-found "Not Found")))
 
 (defn make-app [node]
